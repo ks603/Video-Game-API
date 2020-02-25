501a689ba30d8c9f69888db702a2448979c1d1cc
ks603 <kevin.shaughnessy18@gmail.com>
2020-02-25 17:10:44 -0500

    Updated who can Delete
    now only the user that created the Game can delete that game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -41,7 +41,7 @@ class GamesController < OpenReadController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_game
-      @game = Game.find(params[:id])
+      @game = current_user.games.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
